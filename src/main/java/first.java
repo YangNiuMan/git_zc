060cf64591938e8595130529d21e325a06205dce
yangjie <18226032343@163.com>
2020-05-21 09:16:20 +0800

    idea提交

--- src/main/java/first.java
+++ src/main/java/first.java
@@ -1,6 +1,7 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("今天很好");
- System.out.println("你开心个鬼");
+        System.out.println("你开心个鬼");
+        System.out.println("我也很开心");
     }
 }
